9679650b7b3ff0546ec1b0787800fb1cb60085c9
[Google Jr] <andilembele020@gmail.com>
2020-09-27 00:07:54 +0200

    Refactored and updated

--- build/lib/genuine/fake.py
+++ build/lib/genuine/fake.py
@@ -5,14 +5,19 @@ Created on Thu December  19 14:10:59 2019
 @author: Andile XeroxZen
 """
 
-from . import data_list
+import datetime
+import random
+import string
 from random import choice, randint
-import random, string, datetime
 
-class GenuineFake(object):
-    def __init__(self):
+from . import data_list
 
+
+# noinspection PyUnusedLocal
+class GenuineFake:
+    def __init__(self=None):
         """
+        >>> from genuine.fake import GenuineFake
         >>> GenuineFake.national_id()
         '08-2127709X35'
         >>> GenuineFake.name()
@@ -43,11 +48,9 @@ class GenuineFake(object):
         'Statistician'
         >>> genuine.career()
         'Physician'
-
         """
 
-        return self
-
+    @staticmethod
     def name():
         """
         >>> genuine = GenuineFake
@@ -55,63 +58,63 @@ class GenuineFake(object):
         'Doreen Dlamini'
 
         """
-        name = choice(data_list.common_names) + ' ' + choice(data_list.common_surnames)
-        choice_name = name
-        return choice_name
+        name_data = choice(data_list.common_names) + ' ' + choice(data_list.common_surnames)
+        name = name_data
+        return name
 
-        def __repr__(self):
-            return choice_name
-
+    @staticmethod
     def first_name():
         """
+        >>> from genuine.fake import GenuineFake
+        >>> genuine = GenuineFake
         >>> genuine.first_name()
         'Mongezi'
         """
-        choice_name = data_list.common_names
-        pick = random.choice(choice_name)
-        return pick
+        first_name_data = data_list.common_names
+        first_name = random.choice(first_name_data)
+        return first_name
 
-        def __repr__(self):
-            return pick
-
+    @staticmethod
     def last_name():
         """
+        >>> from genuine.fake import GenuineFake
+        >>> genuine = GenuineFake
         >>> genuine.last_name()
         'Sikhosana'
         """
-        chosen_name = data_list.common_surnames
-        pick = random.choice(chosen_name)
-        return pick
+        last_name_data = data_list.common_surnames
+        last_name = random.choice(last_name_data)
+        return last_name
 
-        def __repr__(self):
-            return pick
-
+    @staticmethod
     def gender():
         possibilities = ["male", "female"]
         sex = random.choice(possibilities)
         return sex
 
-    def date():
-        currentDate = datetime.date.today()
-        # days_date = currentDate
-        return currentDate.strftime("%Y-%m-%d")
-
+    @staticmethod
     def random_date():
-         pos_day = range(1, 31)
-         pos_month = range(1, 13)
-         pos_year = range(1960, 2020)
-        
-         return datetime.date(random.choice(pos_year), random.choice(pos_month), random.choice(pos_day)).strftime("%Y-%m-%d")
+        pos_day = range(1, 31)
+        pos_month = range(1, 13)
+        pos_year = range(1960, 2020)
 
+        return datetime.date(random.choice(pos_year), random.choice(pos_month), random.choice(pos_day)).strftime(
+            "%Y-%m-%d")
+
+    @staticmethod
     def date_of_birth():
-         pos_day = range(1, 31)
-         pos_month = range(1, 13)
-         pos_year = range(1960, 2020)
-         
-         return datetime.date(random.choice(pos_year), random.choice(pos_month), random.choice(pos_day)).strftime("%Y-%m-%d")
+        pos_day = range(1, 31)
+        pos_month = range(1, 13)
+        pos_year = range(1960, 2020)
 
+        return datetime.date(random.choice(pos_year), random.choice(pos_month), random.choice(pos_day)).strftime(
+            "%Y-%m-%d")
+
+    @staticmethod
     def national_id():
         """
+        >>> from genuine.fake import GenuineFake
+        >>> fake = GenuineFake
         >>> fake.national_id()
         '15-516077P91'
         """
@@ -120,17 +123,18 @@ class GenuineFake(object):
         set3 = string.ascii_uppercase
         set4 = string.digits
 
-        issued_set1 = "".join(choice(set1) for x in range(randint(2,2)))
-        issued_set2 = "".join(choice(set2) for x in range(randint(6,7)))
-        issued_set3 = "".join(choice(set3) for x in range(randint(1,1)))
-        issued_set4 = "".join(choice(set4) for x in range(randint(2,2)))
+        i: int
+        issued_set1 = "".join(choice(set1) for i in range(randint(2, 2)))
+        j: int
+        issued_set2 = "".join(choice(set2) for j in range(randint(6, 7)))
+        issued_set3 = "".join(choice(set3) for k in range(randint(1, 1)))
+        x: int
+        issued_set4 = "".join(choice(set4) for x in range(randint(2, 2)))
 
         issued_id = issued_set1 + '-' + issued_set2 + issued_set3 + issued_set4
         return issued_id
 
-        def __repr__(self):
-            return str(self.issued_id)
-
+    @staticmethod
     def address():
         house_no = string.digits
         house_num = "".join(choice(house_no) for x in range(randint(1, 5)))
@@ -141,93 +145,79 @@ class GenuineFake(object):
 
         return chosen_address
 
-        def __repr__(self):
-            return str(self.chosen_address)
-
+    @staticmethod
     def phone_number():
-        country_code = ('+263')
-        network_code = ('77', '73', '71')
+        country_code = '+263'
+        network_code = ['77', '73', '71']
 
         set1 = string.digits
         set2 = string.digits
 
-        code1 = "".join(choice(set1) for x in range(randint(3,3)))
-        code2 = "".join(choice(set2) for x in range(randint(4,4)))
+        code1 = "".join(choice(set1) for i in range(randint(3, 3)))
+        code2 = "".join(choice(set2) for j in range(randint(4, 4)))
 
-        phone_num = country_code + ' ' + random.choice(network_code) + ' ' + code1 + ' ' + code2
-        return phone_num
+        phone_number = country_code + ' ' + random.choice(network_code) + ' ' + code1 + ' ' + code2
+        return phone_number
 
-        def __repr__(self):
-            return str(self.phone_num)
-
+    @staticmethod
     def email():
         first_name = choice(data_list.common_names)
         last_name = choice(data_list.common_surnames)
         user_name = first_name.lower() + last_name.lower()
-        mail_server = ('gmail.com', 'yahoo.com','aol.com', 'outlook.com', 'hotmail.com', 'iCloud.com', 'yahoo.co.uk', 'protonmail.com')
+        mail_server = ('gmail.com', 'yahoo.com', 'aol.com', 'outlook.com', 'hotmail.com', 'iCloud.com', 'yahoo.co.uk',
+                       'protonmail.com')
         mail_address = user_name + '@' + random.choice(mail_server)
         # Want to include _, #s
         return mail_address
 
-        def __repr__(self):
-            return str(self.mail_address)
-
+    @staticmethod
     def medical_aid():
-        data = data_list.medical_aid
-        medical_scheme = random.choice(data)
+        medical_aid_data = data_list.medical_aid
+        medical_scheme = random.choice(medical_aid_data)
         return medical_scheme
 
-        def __repr__(self):
-            return str(self.medical_scheme)
-
+    @staticmethod
     def allergies():
-        data = data_list.allergens
-        allergy = random.choice(data)
+        allergies_data = data_list.allergens
+        allergy = random.choice(allergies_data)
         return allergy
 
-        def __repr__(self):
-            return str(self.allergy)
+    @staticmethod
+    def hospital(self):
+        hospital_data = data_list.hospital
+        hospital = random.choice(hospital_data)
+        return hospital
 
-    def hospital():
-        data = data_list.hospital
-        choice = random.choice(data)
-        return choice
+    @staticmethod
+    def career(self):
+        career_data = data_list.career
+        career = random.choice(career_data)
+        return career
 
-        def __repr__(self):
-            return str(self.choice)
+    @staticmethod
+    def career_position(self):
+        career_position_data = data_list.career_position
+        career_position = random.choice(career_position_data)
+        return career_position
 
-    def career():
-        data = data_list.career
-        choice = random.choice(data)
-        return choice
+    @staticmethod
+    def company(self):
+        company_data = data_list.company
+        company = random.choice(company_data)
+        return company
 
-        def __repr__(self):
-            return str(self.choice)
+    @staticmethod
+    def medical_professions(self):
+        profession_data = data_list.medical_roles
+        profession = random.choice(profession_data)
+        return profession
 
-    def career_position():
-        data = data_list.career_position
-        choice = random.choice(data)
-        return choice
-
-        def __repr__(self):
-            return str(self.choice)
-
-    def company():
-        data = data_list.company
-        choice = random.choice(data)
-        return choice
-
-        def __repr__(self):
-            return str(self.choice)
-
-    def medical_professions():
-        data = data_list.medical_roles
-        choice = random.choice(data)
-        return choice
-
-        def __repr__(self):
-            return str(self.choice)
+    @staticmethod
+    def date():
+        current_date = datetime.date.today()
+        # days_date = current_date
+        return current_date.strftime("%Y-%m-%d")
 
 
 if __name__ == '__main__':
-   data = GenuineFake
+    data = GenuineFake
