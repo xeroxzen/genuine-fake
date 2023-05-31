95295d274bd9366e4af7e5cc02675c2a64532f06
Google Jr <andilembele020@gmail.com>
2023-05-31 15:22:54 +0200

    fix: version clashes #2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.11",
     ],
     packages=["genuine"],
     include_package_data=True,
